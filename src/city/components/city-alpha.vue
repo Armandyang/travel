bf8472f3c640c9764ab4fcb1eab72fde12204172
Jack <2587629620@qq.com>
2019-02-28 20:38:07 +0800

    修改了city-alpha

--- src/city/components/city-alpha.vue
+++ src/city/components/city-alpha.vue
@@ -41,7 +41,7 @@
 			handleTouchMove(e){
 				if( this.touchStatus ){
 					const startY = this.$refs['A'][0].offsetTop
-					const touchY = e.touches[0].clientY - startY
+					const touchY = e.touches[0].clientY - 79
 					const index = Math.floor((touchY - startY)/20)
 					if( index>=0 && index<this.letters.length){
 						this.$emit('change', this.letters[index])
